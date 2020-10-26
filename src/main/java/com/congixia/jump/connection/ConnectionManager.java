4af42d35369534018047c236bd1e398b659de875
JCTGY <fan3510@gmail.com>
2020-10-26 08:56:20 -0700

    add ConnectionManager

---
+++ src/main/java/com/congixia/jump/connection/ConnectionManager.java
@@ -0,0 +1,40 @@
+package com.congixia.jump.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+	private static Connection connection = null;
+
+	private static final String MACURL = "jdbc:mysql://localhost:3306/library?serverTimezone=EST5EDT";
+	private static final String WINURL = "jdbc:mysql://localhost:3306/library";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "Root@123";
+
+	private static void makeConnection() {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(MACURL, USERNAME, PASSWORD);
+			System.out.println("Connected");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static Connection getConnection() {
+
+		if (connection == null) {
+			makeConnection();
+		}
+
+		return connection;
+	}
+}
