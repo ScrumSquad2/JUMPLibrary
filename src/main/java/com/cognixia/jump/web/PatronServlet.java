4cfd6925649154279f8310b983e3b8e206e37fd6
JCTGY <fan3510@gmail.com>
2020-10-27 14:22:55 -0700

    remove jsp test and update patron servlet

--- src/main/java/com/cognixia/jump/web/PatronServlet.java
+++ src/main/java/com/cognixia/jump/web/PatronServlet.java
@@ -1,10 +1,12 @@
 package com.cognixia.jump.web;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,17 +14,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.cognixia.jump.connection.ConnectionManager;
+import com.cognixia.jump.dao.BookCheckoutDAO;
+import com.cognixia.jump.dao.BookCheckoutDAOImpl;
+import com.cognixia.jump.dao.ItemNotFoundInDatabaseException;
+import com.cognixia.jump.dao.PatronDAO;
+import com.cognixia.jump.dao.PatronDAOImpl;
+import com.cognixia.jump.model.BookCheckout;
+import com.cognixia.jump.model.Patron;
 
 @WebServlet("/patron")
 public class PatronServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-    
-	private Connection conn = ConnectionManager.getConnection();
-
+    private PatronDAO patronDAO;
+    private BookCheckoutDAO bookCheckoutDAO;
+	
 	@Override
 	public void init() {
-		//TODO initialize the DAO class
+		
+		patronDAO = new PatronDAOImpl();
+		bookCheckoutDAO = new BookCheckoutDAOImpl();
 	}
 	
 	@Override
@@ -34,14 +45,18 @@ public class PatronServlet extends HttpServlet {
 			insertPatron(request, response);
 			break;
 		case "/updatePatron":
+			updatePatron(request, response);
 			break;
 		case "/listCurrent":
+			listCurrent(request, response);
 			break;
 		case "/listReturned":
 			break;
 		case "/addCheckout":
+			addCheckout(request, response);
 			break;
 		case "/returnCheckout":
+			returnCheckout(request, response);
 			break;
 		default:
 			response.sendRedirect("/");
@@ -53,30 +68,67 @@ public class PatronServlet extends HttpServlet {
 	private void insertPatron(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
-		final String stat = "INSERT INTO patron(first_name, last_name, username, password, account_frozen) \n"
-				+ "	values(?, ?, ?, ?, false)";
-		
-		
 		String firstName = request.getParameter("firstName");
 		String lastName = request.getParameter("lastName");
 		String userName = request.getParameter("userName");
 		String password = request.getParameter("password");
+		boolean accountFrozen = false;
 		
+		if (patronDAO.addPatron(
+				new Patron(0, firstName, lastName, userName, password, accountFrozen)))
+			System.out.println("Sucess add " + userName);
+	}
+	
+	private void updatePatron(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		int patronId = Integer.parseInt(request.getParameter("patronId"));
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		boolean accountFrozen = false;
 		
-		try (PreparedStatement ptsmt = conn.prepareStatement(stat)) {
-			ptsmt.setString(1, firstName);
-			ptsmt.setString(2, lastName);
-			ptsmt.setString(3, userName);
-			ptsmt.setString(4, password);
-			int count = ptsmt.executeUpdate();
-			if (count != 0) {
-				System.out.println("sucess add patron");
-			}
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		if (patronDAO.updatePatron(
+				new Patron(patronId, firstName, lastName, userName, password, accountFrozen)))
+			System.out.println("Sucess update " + userName);
+	}
+	
+	private void listCurrent(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		int patronId = Integer.parseInt(request.getParameter("patronId"));
+		List<BookCheckout> bookCheckouts;
+		try {
+			bookCheckouts = bookCheckoutDAO.getAllCheckouts(patronDAO.getPatronById(patronId));
+			System.out.println("bookCheckouts: " + bookCheckouts);
+			
+			request.setAttribute("bookCheckouts", bookCheckouts);
+			// TODO change to correct jsp
+			RequestDispatcher dispatcher = request.getRequestDispatcher("product-list.jsp");
+			System.out.println("send");
+			dispatcher.forward(request, response);	
+		} catch (ItemNotFoundInDatabaseException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void addCheckout(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		int patronId = Integer.parseInt(request.getParameter("patronId"));
+		String isbn = request.getParameter("isbn");
+		Date checkoutDate = new Date(System.currentTimeMillis());
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.DATE, 30);
+		Date dueDate = new Date(c.getTimeInMillis());
+		if (bookCheckoutDAO.addBookCheckout(
+				new BookCheckout(0, patronId, isbn, checkoutDate, dueDate, null)))
+			System.out.println("Sucess add BookCheckout " + isbn);
+	}
+	
+	private void returnCheckout(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("checkoutID"));
+		if (bookCheckoutDAO.returnBookCheckout(new BookCheckout(id, 0, null, null, null, null)))
+			System.out.println("Sucess returned checkout " + id);
 	}
 	
 	@Override
@@ -90,7 +142,7 @@ public class PatronServlet extends HttpServlet {
 		
 		//TODO get the Connection and destroy
 		try {
-			conn.close();
+			ConnectionManager.getConnection().close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
